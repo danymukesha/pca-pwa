23c79c3b69fe62c25dc484ac3857188fd0983d88
danymukesha <danymukesha@gmail.com>
2024-01-08 16:04:03 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pca_pwa',
+    version='1.0.3',
+    description='A package for PCA analysis with Flask application',
+    author='Dany Mukesha',
+    author_email='danymukesha@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'scikit-learn',
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'seaborn',
+        'adjustText',
+        'scipy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pca_pwa_run=pca_pwa:main',
+        ],
+    },
+)
